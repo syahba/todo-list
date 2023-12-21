9f9d051549a31cd2b8c6b2d15da40076ab1dca72
syahba <ksyaahba@gmail.com>
2023-12-21 23:59:32 +0700

    edit file

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todo from './slice/todo-slice';
-import modal from './slice/modal-slice';
 
 const store = configureStore({
   reducer: {
-    todo,
-    modal
+    todo
   }
 });
 
