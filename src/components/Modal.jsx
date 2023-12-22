7b76f2983aa068680201d5cf65aa775cd10041a8
syahba <ksyaahba@gmail.com>
2023-12-22 23:33:10 +0700

    fix modal styling

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addTask, editTask } from "../redux/slice/todo-slice";
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import '../styles/Modal.css'
 
 function Modal({ setIsOpen, activity, data }) {
   const dispatch = useDispatch();
@@ -10,16 +13,13 @@ function Modal({ setIsOpen, activity, data }) {
   return (
     <div>
       {/* unfinished styling */}
-      <div className="fixed z-10 left-0 top-0 h-full w-full">
-        <div className="flex items-center justify-center">
-          <input type="text" className="h-11 w-1/2 text-sm pl-4 rounded-lg shadow-[4px_4px_8px_0px_rgba(0,0,0,0.10)]" value={input} onChange={(e) => setInput(e.target.value)} />
-          <button className="bg-orange-400 py-3 px-6 ml-5 rounded-lg text-sm shadow-[4px_4px_8px_0px_rgba(0,0,0,0.10)]" onClick={() => {
-            activity === 'Add' ? dispatch(addTask(input)) : dispatch(editTask(data.id, input));
-            setIsOpen(false);
-          }}>{activity}</button>
-        </div>
-
-        <div className="h-screen w-screen bg-black bg-opacity-50" onClick={() => setIsOpen(false)}></div>
+      <div className="modal">
+        <input type="text" className="modal-input" value={input} onChange={(e) => setInput(e.target.value)} />
+        <button className="add-btn" onClick={() => {
+          activity === 'Add' ? dispatch(addTask(input)) : dispatch(editTask(data.id, input));
+          setIsOpen(false);
+        }}>{activity}</button>
+        <FontAwesomeIcon className="fa-lg close-btn" icon={faXmark} style={{ color: '#fff' }} onClick={() => setIsOpen(false)} />
       </div>
     </div>
   );
